3cf57b95cf3d32b82c9c79a646ea74341fbd4d5e
Szymon Bernal <szymon.bernal@gmail.com>
2018-01-14 16:14:30 +0100

    getters for current measurements

--- src/checkAIR/Airly/ExtendedMeasurements.java
+++ src/checkAIR/Airly/ExtendedMeasurements.java
@@ -10,6 +10,53 @@ public class ExtendedMeasurements {
 
     private DatedMeasurements[] history = new DatedMeasurements[] {};
 
+
+
+    public double getCurrentAirQualityIndex() {
+        return currentMeasurements.getAirQualityIndex();
+    }
+
+    public double getCurrentHumidity() {
+        return currentMeasurements.getHumidity();
+    }
+
+    public String getCurrentMeasurementTime() {
+        return currentMeasurements.getMeasurementTime();
+    }
+
+    public double getCurrentPm1() {
+        return currentMeasurements.getPm1();
+    }
+
+    public double getCurrentPm10() {
+        return currentMeasurements.getPm10();
+    }
+
+    public double getCurrentPm25() {
+        return currentMeasurements.getPm25();
+    }
+
+    public double getCurrentPollutionLevel() {
+        return currentMeasurements.getPollutionLevel();
+    }
+
+    public double getCurrentPressure() {
+        return currentMeasurements.getPressure();
+    }
+
+    public double getCurrentTemperature() {
+        return currentMeasurements.getTemperature();
+    }
+
+    public double getCurrentWindDirection() {
+        return currentMeasurements.getWindDirection();
+    }
+
+    public double getCurrentWindSpeed() {
+        return currentMeasurements.getWindSpeed();
+    }
+
+
     @Override
     public String toString() {
         return
