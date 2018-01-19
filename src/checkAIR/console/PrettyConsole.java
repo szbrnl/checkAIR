add9583680c06c1a404da2c441c9ec217cebd502
Szymon Bernal <szymon.bernal@gmail.com>
2018-01-19 03:01:30 +0100

    cleanup method for adding frames

--- src/checkAIR/console/PrettyConsole.java
+++ src/checkAIR/console/PrettyConsole.java
@@ -11,27 +11,11 @@ public class PrettyConsole {
         int width = 100;
         int height = 20;
 
-        Frame namesFrame = new Frame(true);
-        namesFrame.add("pm25");
-        namesFrame.add("2222222222222", Color.TextRed);
-        namesFrame.add("2222222222222222");
-
-        Frame valueFrames = new Frame(false);
-        valueFrames.add("odczyt1");
-        valueFrames.add("odczyt234234");
-        valueFrames.add("odczytssdaadasasads234", Color.BackgroundBlue);
-
-
         window = new Window(width, height, 40, firstTitle, secondTitle);
-        window.addColumn(namesFrame);
-        window.addColumn(valueFrames);
-        window.addColumn(valueFrames);
-        window.addColumn(valueFrames);
-        window.addColumn(valueFrames);
-        window.addColumn(valueFrames);
+    }
 
-
-
+    public void addFrame(Frame frame) {
+        window.addColumn(frame);
     }
 
 
