aa9212200424dafeb1f6d1e31636205ef2c8b891
Szymon Bernal <szymon.bernal@gmail.com>
2018-01-19 18:52:00 +0100

    added constructor with message

--- src/checkAIR/airly/NotProvidedException.java
+++ src/checkAIR/airly/NotProvidedException.java
@@ -4,4 +4,8 @@ public class NotProvidedException extends Exception {
     public NotProvidedException() {
         super();
     }
+
+    public NotProvidedException(String message) {
+        super(message);
+    }
 }
