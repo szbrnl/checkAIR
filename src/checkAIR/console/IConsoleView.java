be484c8048ee102ce443294ece792fd717ca8cde
Szymon Bernal <szymon.bernal@gmail.com>
2018-01-19 19:34:44 +0100

    changed IConsoleView interface

--- src/checkAIR/console/IConsoleView.java
+++ src/checkAIR/console/IConsoleView.java
@@ -1,22 +1,11 @@
 package checkAIR.console;
 
 
+import checkAIR.airly.MeasurementType;
+
 import java.util.List;
 
+
 public interface IConsoleView {
-
-    void add(String name, String value, Color nameColor, Color valueColor);
-    void add(String name, String value);
-
-    List<StringBuilder> getContent();
-    List<Integer> getContentLinesLengths();
-
-    void addFrame(Frame frame, int posX, int posY);
-
-    void addImage(StringBuilder image);
-
-    int getWidth();
-
-    int getHeight();
-
+    List<Frame> getColumns();
 }
