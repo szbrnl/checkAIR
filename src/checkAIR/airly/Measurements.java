b8d5d8bd40a118fd32b526140f3a66f57f8a3ca4
Szymon Bernal <szymon.bernal@gmail.com>
2018-01-19 18:52:52 +0100

    removed unnecessary fields

--- src/checkAIR/airly/Measurements.java
+++ src/checkAIR/airly/Measurements.java
@@ -12,23 +12,13 @@ public class Measurements {
 
     private Double humidity;
 
-    private String measurementTime;
-
-    private Double pm1;
-
     private Double pm10;
 
     private Double pm25;
 
-    private Double pollutionLevel;
-
     private Double pressure;
 
     private Double temperature;
-
-    private Double windDirection;
-
-    private Double windSpeed;
 
 
     public Double getAirQualityIndex() {
@@ -39,24 +29,12 @@ public class Measurements {
         return humidity;
     }
 
-    public String getMeasurementTime() {
-        return measurementTime;
-    }
-
-    public Double getPm1() {
-        return pm1;
-    }
-
     public Double getPm10() {
         return pm10;
     }
 
     public Double getPm25() {
         return pm25;
-    }
-
-    public Double getPollutionLevel() {
-        return pollutionLevel;
     }
 
     public Double getPressure() {
@@ -67,28 +45,15 @@ public class Measurements {
         return temperature;
     }
 
-    public Double getWindDirection() {
-        return windDirection;
-    }
-
-    public Double getWindSpeed() {
-        return windSpeed;
-    }
-
     @Override
     public String toString() {
         return
                 "airQualityIndex=" + airQualityIndex +
-                        "\nhumidity=" + humidity +
-                        "\nmeasurementTime='" + measurementTime + '\'' +
-                        "\npm1=" + pm1 +
                         "\npm10=" + pm10 +
                         "\npm25=" + pm25 +
-                        "\npollutionLevel=" + pollutionLevel +
                         "\npressure=" + pressure +
+                        "\nhumidity=" + humidity +
                         "\ntemperature=" + temperature +
-                        "\nwindDirection=" + windDirection +
-                        "\nwindSpeed=" + windSpeed +
                         "\n";
     }
 }
