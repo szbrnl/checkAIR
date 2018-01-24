e5115ee61ad2dbe71c02082b42273b6bb74717de
Szymon Bernal <szymon.bernal@gmail.com>
2018-01-24 17:19:34 +0100

    added asciiartnumber

---
+++ src/checkAIR/console/AsciiArtNumber.java
@@ -0,0 +1,14 @@
+package checkAIR.console;
+
+import java.util.List;
+
+public class AsciiArtNumber {
+    private List<StringBuilder> lines;
+
+    private int width;
+
+
+    public AsciiArtNumber (int number) {
+
+    }
+}
