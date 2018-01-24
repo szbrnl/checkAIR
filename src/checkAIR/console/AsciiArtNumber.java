7b9cb21bfecdb7e92b5861850605b2becc48e5ef
Szymon Bernal <szymon.bernal@gmail.com>
2018-01-24 17:46:20 +0100

    added number generator

--- src/checkAIR/console/AsciiArtNumber.java
+++ src/checkAIR/console/AsciiArtNumber.java
@@ -1,14 +1,127 @@
 package checkAIR.console;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AsciiArtNumber {
     private List<StringBuilder> lines;
 
-    private int width;
+    public AsciiArtNumber(int number) {
+        generateNumber(number);
+    }
+
+    private void generateNumber(int number) {
+
+        String digit = getDigit(number%10);
+        number/=10;
+
+        lines = new LinkedList<>(
+                Arrays.stream(
+                        digit.split("\n"))
+                        .map(x -> new StringBuilder(x))
+                        .collect(Collectors.toList())
+        );
+
+        while (number > 0) {
+            digit = getDigit(number%10);
+            List<String> digitLines = Arrays.asList(digit.split("\n"));
+
+            for (int i=0; i<digitLines.size(); i++) {
+                StringBuilder currentLine = lines.get(i);
+                currentLine.append(digitLines.get(i));
+            }
+
+        }
+
+    }
+
+    public int getWidth() {
+        return lines.get(0).length();
+    }
+
+    public List<StringBuilder> getLines() {
+        return lines;
+    }
 
 
-    public AsciiArtNumber (int number) {
+    private String getDigit(int digit) {
+        switch (digit) {
+            case 1:
+                return " ██╗\n" +
+                        "███║\n" +
+                        "╚██║\n" +
+                        " ██║\n" +
+                        " ██║\n" +
+                        " ╚═╝\n";
+            case 2:
+                return "██████╗ \n" +
+                        "╚════██╗\n" +
+                        " █████╔╝\n" +
+                        "██╔═══╝ \n" +
+                        "███████╗\n" +
+                        "╚══════╝\n";
+            case 3:
+                return "██████╗ \n" +
+                        "╚════██╗\n" +
+                        " █████╔╝\n" +
+                        " ╚═══██╗\n" +
+                        "██████╔╝\n" +
+                        "╚═════╝ \n";
+            case 4:
+                return "██╗  ██╗\n" +
+                        "██║  ██║\n" +
+                        "███████║\n" +
+                        "╚════██║\n" +
+                        "     ██║\n" +
+                        "     ╚═╝\n";
+            case 5:
+                return "███████╗\n" +
+                        "██╔════╝\n" +
+                        "███████╗\n" +
+                        "╚════██║\n" +
+                        "███████║\n" +
+                        "╚══════╝\n";
+            case 6:
+                return " ██████╗ \n" +
+                        "██╔════╝ \n" +
+                        "███████╗ \n" +
+                        "██╔═══██╗\n" +
+                        "╚██████╔╝\n" +
+                        " ╚═════╝ \n";
 
+            case 7:
+                return "███████╗\n" +
+                        "╚════██║\n" +
+                        "    ██╔╝\n" +
+                        "   ██╔╝ \n" +
+                        "   ██║  \n" +
+                        "   ╚═╝  \n";
+
+            case 8:
+                return " █████╗ \n" +
+                        "██╔══██╗\n" +
+                        "╚█████╔╝\n" +
+                        "██╔══██╗\n" +
+                        "╚█████╔╝\n" +
+                        " ╚════╝ \n";
+            case 9:
+                return " █████╗  \n" +
+                        "██╔══██╗ \n" +
+                        "╚██████║ \n" +
+                        " ╚═══██║ \n" +
+                        " █████╔╝ \n" +
+                        " ╚════╝  \n";
+            case 0:
+                return " ██████╗ \n" +
+                        "██╔═████╗\n" +
+                        "██║██╔██║\n" +
+                        "████╔╝██║\n" +
+                        "╚██████╔╝\n" +
+                        " ╚═════╝ \n";
+            default:
+                return "";
+        }
     }
 }
